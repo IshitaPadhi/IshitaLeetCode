477a855f08420010e977057695ad09aeb06d7f13
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-07-10 23:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (76.87%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,31 +1,36 @@
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        int col[]=new int[graph.length];
-        for(int i=0;i<col.length;i++){
-            col[i]=-1; // no colour
-        }
-        Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<graph.length;i++){
-            if(col[i]==-1){//unvisited hain toh 
-                q.add(i);
-                col[i]=0;//yellow
-                while(!q.isEmpty()){
-                    int curr=q.remove();
-                    for(int j=0;j<graph[curr].length;j++){
-                        int neighbour=graph[curr][j];
-                        if(col[neighbour]==-1){//case 1 ->no color
+    public boolean dfs(int graph[][],int curr,int col[]){
+                for(int j=0;j<graph[curr].length;j++){
+                    int neighbour=graph[curr][j];
+                    if(col[neighbour]==-1){
                         int nextCol=(col[curr]==0)?1:0;
                         col[neighbour]=nextCol;
-                        q.add(neighbour);
-
-                        }else if(col[neighbour]==col[curr]){
-                            return false; // not birpartite adj walo ka same color
+                        if(!dfs(graph,neighbour,col)){
+                            return false;
                         }
+                    }else if(col[neighbour]==col[curr]){
+                        return false;
                     }
                 }
+                return true;
+            
+        }
+        
+
+    
+    public boolean isBipartite(int[][] graph) {
+        int col[]=new int[graph.length];
+        for(int i=0;i<graph.length;i++){
+            col[i]=-1;
+        }
+        for(int i=0;i<graph.length;i++){
+            if(col[i]==-1){
+                col[i]=0;
+               if (!dfs(graph,i,col)){
+                return false;
+               }
             }
         }
-        return true;//if graph doesn't have cycle->by default bypartite (extra info)
-        
+        return true;
     }
 }
