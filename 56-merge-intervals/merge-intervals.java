0b01409b37e83629df9ccb6c014cbfc6607725e9
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-07-12 23:53:27 +0530

    Time: 8 ms (91.94%) | Memory: 49.2 MB (27.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
+    public int[][] merge(int[][] intervals) {//nlogn for sorting +O(n^2), sc is O(n)
         int n=intervals.length;
         List<int[]>ans=new ArrayList<>();
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            //if previous megred interval covers the current one then we skip it for ex [1,6] already covers [2,5] toh we won't need to merge
-            if(!ans.isEmpty()&& end<=ans.get(ans.size()-1)[1]) //most recent ka we want to compare the end 
-            {continue;
-            }
-            for(int j=i+1;j<n;j++){
-                //if there is an overlap then update the end
-                if(intervals[j][0]<=end){
-                    end=Math.max(end,intervals[j][1]);
-
+        ans.add(new int[]{intervals[0][0],intervals[0][1]}); 
+        //add the first interval in the list to avoid nested loops then start from the second merged list
+            for(int i=1;i<n;i++){
+                //current interval
+                int start=intervals[i][0];
+                int end=intervals[i][1];
+                 // Answer list ka last merged interval
+                int []lastMerged=ans.get(ans.size()-1);
+                //if there is an overlap like (1,3) and (2,6) then we will merge it (1,6)
+                if(start<=lastMerged[1]){
+                    // End ko jitna bada ho utna update kar do
+                    lastMerged[1]=Math.max(end,lastMerged[1]);
                 }else{
-                    //if there is not overlap then break
-                    break;
+                    //if there is no overlap we add it in the final list
+                    ans.add(new int[]{start,end});
                 }
-
+ 
             }
-            //we'll add the final merged interval to the ans list
-            ans.add(new int[]{start,end});
-            
+           
+              // List<int[]> ko int[][] mein convert kar do
+        return ans.toArray(new int[ans.size()][]);
             
         }
 
-    // List<int[]> ko int[][] mein convert kar do
-        return ans.toArray(new int[ans.size()][]);
+  
     }
-}
