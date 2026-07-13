7699da1f8295c3bae5d2e1181f8b729321183a00
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-07-13 12:28:46 +0530

    Time: 1 ms (26.37%) | Memory: 43.4 MB (95.12%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -24,7 +24,7 @@
 
     }
 }*/
-class Solution{
+/*class Solution{
     public void swap(int nums1[],int nums2[],int i, int j){
         int temp=nums1[i];
         nums1[i]=nums2[j];
@@ -56,4 +56,64 @@ class Solution{
         }
 
      }
+}*/
+/*1. Swap + Sort Approach (ye wala code)
+Swap loop: O(min(m, n))
+Arrays.sort(nums1, 0, m) → O(m log m)
+Arrays.sort(nums2) → O(n log n)
+Copy back → O(n)
+
+Overall TC:
+
+O(m log m + n log n)
+
+(Ye dominate karta hai, swap aur copy ignore ho jaate hain.)
+
+SC: O(1)*/ //-> approach 1 
+/*2. Gap Method
+
+Har pass me poore array ko traverse karte hain → O(m+n)
+
+Gap approximately half hota rehta hai:
+
+len → len/2 → len/4 → ... → 1
+
+Total passes = log(m+n)
+
+Overall TC:
+
+O((m+n) log(m+n))*/ //GAP METHOD -> APPROACH 2
+
+
+class Solution{
+    public void swapIfGreater(int nums1[],int ind1,int ind2){
+        if(nums1[ind1]>nums1[ind2]){
+            int temp=nums1[ind1];
+            nums1[ind1]=nums1[ind2];
+            nums1[ind2]=temp;
+        }
+    }
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+         // pehle nums2 ke elements nums1 ke end me copy kar do
+         for(int i=0;i<n;i++){
+            nums1[i+m]=nums2[i];
+         }
+         int len=m+n;
+          // initial gap = ceil(len/2)
+         int gap=(len/2)+(len%2);
+         while(gap>0){
+            int left=0;
+            int right=left+gap;
+              // gap maintain karte hue compare karte jao
+              while(right<len){
+                  // agar left wala bada hai toh swap kar do
+                  swapIfGreater(nums1,left,right);
+                  left++;
+                  right++;
+              }
+              if(gap==1)break;
+            gap = (gap / 2) + (gap % 2);
+         }
+
+    }
 }
