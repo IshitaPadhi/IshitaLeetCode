e20210b0b661944714dc842190cdb861760806d6
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-04 23:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (29.50%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int solve(int i, int j,int dp[][]) {
-        if(i<0 || j<0){
-            return 0;
-        }
+    public int solve(int i,int j,int dp[][]){
         if(i==0 && j==0){
             return 1;
         }
-        if(dp[i][j]!=-1){//if alr calculated hai
-        return dp[i][j];
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         int up=solve(i-1,j,dp);
         int left=solve(i,j-1,dp);
-        return dp[i][j]=up+left;
+        dp[i][j]=up+left;
+        return dp[i][j];
     }
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
@@ -20,5 +21,4 @@ class Solution {
         }
         return solve(m-1,n-1,dp);
     }
-
-    }
+}
