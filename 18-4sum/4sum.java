91b9b1c5943d7e19b641f49f68d838347ef58948
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-12 17:46:03 +0530

    Time: 21 ms (41.67%) | Memory: 46.3 MB (14.80%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -25,30 +25,62 @@
       
 // }
 // }
+// class Solution{
+//      public List<List<Integer>> fourSum(int[] nums, int target){
+//         int n=nums.length;
+//             //to store quadruplets
+//         Set<List<Integer>> st = new HashSet<>();
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//             //to store (i,j) pair
+//                 Set<Long> hashset=new HashSet<>();
+//                 for(int k=j+1;k<nums.length;k++){
+//                    long sum=(long)nums[i]+nums[j]+nums[k];
+//                    long fourth=(long)target-sum;
+//                    if(hashset.contains(fourth)){
+//                     List<Integer> temp= Arrays.asList(
+//                         nums[i],nums[j],nums[k],(int) fourth
+//                     );
+//                     Collections.sort(temp);
+//                     st.add(temp);
+//                    }
+//                    hashset.add((long)nums[k]);
+
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(st);
+//      }
+// }
 class Solution{
-     public List<List<Integer>> fourSum(int[] nums, int target){
+      public List<List<Integer>> fourSum(int[] nums, int target){
         int n=nums.length;
-            //to store quadruplets
-        Set<List<Integer>> st = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-            //to store (i,j) pair
-                Set<Long> hashset=new HashSet<>();
-                for(int k=j+1;k<nums.length;k++){
-                   long sum=(long)nums[i]+nums[j]+nums[k];
-                   long fourth=(long)target-sum;
-                   if(hashset.contains(fourth)){
-                    List<Integer> temp= Arrays.asList(
-                        nums[i],nums[j],nums[k],(int) fourth
-                    );
-                    Collections.sort(temp);
-                    st.add(temp);
-                   }
-                   hashset.add((long)nums[k]);
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=(i+1) && nums[j]==nums[j-1])continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+
+                    if(sum==target){
+                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                    
+                    while(k<l && nums[k]==nums[k-1])k++;
+                    while(k<l && nums[l]==nums[l+1])l--;
+                    }else if(sum<target){k++;}
+                else{ l--;}
+
 
                 }
             }
         }
-        return new ArrayList<>(st);
-     }
+        return ans;
+      }
 }
