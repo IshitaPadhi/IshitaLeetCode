d34af364adfed2689367f4895b6fe6a40abf3671
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-14 16:00:09 +0530

    Time: 30 ms (74.00%) | Memory: 95.6 MB (39.67%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -23,26 +23,54 @@
 
 //     }
 // }
+// class Solution{//TC-nlogn(sorting),sc->o(1)
+//     public int longestConsecutive(int nums[]){
+//         int longest=0;
+//         int cntcurr=0;
+//         int lastSmaller=Integer.MIN_VALUE;
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]-1==lastSmaller){
+//                 cntcurr=cntcurr+1;
+//                 lastSmaller=nums[i];
+//             }
+//             else if(nums[i]!=lastSmaller){
+//                 //we won't do anything we will continue
+//                 cntcurr=1; //counter reset
+//                 lastSmaller=nums[i];
+
+//             }
+//             longest=Math.max(cntcurr,longest);
+//         }
+//         return longest;
+
+//     }
+// }
 class Solution{
-    public int longestConsecutive(int nums[]){
-        int longest=0;
-        int cntcurr=0;
-        int lastSmaller=Integer.MIN_VALUE;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]-1==lastSmaller){
-                cntcurr=cntcurr+1;
-                lastSmaller=nums[i];
-            }
-            else if(nums[i]!=lastSmaller){
-                //we won't do anything we will continue
-                cntcurr=1; //counter reset
-                lastSmaller=nums[i];
+     public int longestConsecutive(int nums[]){
+        int n=nums.length;
+        if(n==0)return 0;
+        int longest=1;
+        HashSet<Integer>st=new HashSet<>();
+        //putting all elements in the set
+        for(int i=0;i<n;i++){
+            st.add(nums[i]);
+        }
+        //finding the start of every sequence
+        for(int it:st){
+            if(!st.contains(it-1)){//if it's prev doesn't exist so it is the starting point
+            int count=1;
+            int x=it;
+            //till the time we don't find the next consecutive element
+            while(st.contains(x+1)){
+                x=x+1;
+                count++;
 
             }
-            longest=Math.max(cntcurr,longest);
+            longest=Math.max(count,longest);
+
+            }
         }
         return longest;
-
-    }
+     }
 }
