b14e9548f5ce6746b8e6c158a36db6d4d2913d01
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-26 01:10:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (16.11%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,25 +8,38 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    //TC->O(N+N/2)and sc->O(1)
-    public ListNode middleNode(ListNode head) {
-        ListNode temp=head;
-        int count=0;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
-        }
-        double midNode=(count/2)+1;
-        temp=head;
-        while(temp!=null){
-            midNode=midNode-1;
-            if(midNode==0){
-                break;
-            }
-            temp=temp.next;
-        }
-        return temp;
+// class Solution {
+//     //TC->O(N+N/2)and sc->O(1)
+//     public ListNode middleNode(ListNode head) {
+//         ListNode temp=head;
+//         int count=0;
+//         while(temp!=null){
+//             count++;
+//             temp=temp.next;
+//         }
+//         double midNode=(count/2)+1;
+//         temp=head;
+//         while(temp!=null){
+//             midNode=midNode-1;
+//             if(midNode==0){
+//                 break;
+//             }
+//             temp=temp.next;
+//         }
+//         return temp;
 
-    }
+//     }
+// }
+class Solution{
+    //O(n/2)->TC
+    //O(1)->SC
+     public ListNode middleNode(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+     }
 }
