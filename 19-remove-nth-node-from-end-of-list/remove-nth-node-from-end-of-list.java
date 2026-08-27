b9c6c446739220030de46e95f44c950ee14b1f4a
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-27 19:31:55 +0530

    Refactor removeNthFromEnd method for efficiency

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,35 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        int cnt=0; 
-        ListNode temp=head;
-        //we will compute the length of the LL
-        while(temp!=null){
-            cnt++;
-            temp=temp.next;
-        }//if from the end we want to delete the nth node
-        //that is the head node
-        if(cnt==n){
-            ListNode newHead=head.next;
-            return newHead;
-        }
-        //from the start it will be cnt-n+1 but we want to go to
-        //it's prev so it will be cnt-n
-        int res=cnt-n;
-        ListNode temp1=head;
-        while(temp1!=null){
-            res--; //we will keep a counter till we reach the prev of delNode
-            if(res==0){
-                break;
-            }
-            temp1=temp1.next;
-        }
-        ListNode delNode=temp1.next;
-        temp1.next=temp1.next.next;
-        return head;
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         int cnt=0; 
+//         ListNode temp=head;
+//         //we will compute the length of the LL
+//         while(temp!=null){
+//             cnt++;
+//             temp=temp.next;
+//         }//if from the end we want to delete the nth node
+//         //that is the head node
+//         if(cnt==n){
+//             ListNode newHead=head.next;
+//             return newHead;
+//         }
+//         //from the start it will be cnt-n+1 but we want to go to
+//         //it's prev so it will be cnt-n
+//         int res=cnt-n;
+//         ListNode temp1=head;
+//         while(temp1!=null){
+//             res--; //we will keep a counter till we reach the prev of delNode
+//             if(res==0){
+//                 break;
+//             }
+//             temp1=temp1.next;
+//         }
+//         ListNode delNode=temp1.next;
+//         temp1.next=temp1.next.next;
+//         return head;
 
         
+//     }
+// }
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast=head;
+        ListNode slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null)return head.next;
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        ListNode delnode=slow.next;
+        slow.next=slow.next.next;
+        return head;
     }
 }
