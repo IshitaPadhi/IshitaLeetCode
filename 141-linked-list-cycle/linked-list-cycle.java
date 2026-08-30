031121e0aca18b83afbbbb611cb2eba4f8737eff
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-08-30 18:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (73.18%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,17 +9,32 @@
  *     }
  * }
  */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        HashMap<ListNode,Integer>map=new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-             // If node is already present, cycle exists
-            if(map.containsKey(temp)){
+// public class Solution {
+//TC- O(n) and SC-O(n)
+//     public boolean hasCycle(ListNode head) {
+//         HashMap<ListNode,Integer>map=new HashMap<>();
+//         ListNode temp=head;
+//         while(temp!=null){
+//              // If node is already present, cycle exists
+//             if(map.containsKey(temp)){
+//                 return true;
+//             }
+//             map.put(temp,1);
+//             temp=temp.next;
+//         }
+//         return false;
+//     }
+// }
+public class Solution{
+    public boolean hasCycle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            map.put(temp,1);
-            temp=temp.next;
         }
         return false;
     }
