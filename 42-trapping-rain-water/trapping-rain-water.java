667d6d6d8a913a638c1deca0eb5dd01791f4d95a
IshitaPadhi <ishitapadhi2005@gmail.com>
2026-09-17 02:17:12 +0530

    Time: 1 ms (56.06%) | Memory: 47.3 MB (94.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int[] prefixMax(int arr[]){
-        int n=arr.length;
-        int prefix[]=new int[n];
-        prefix[0]=arr[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=Math.max(prefix[i-1],arr[i]);
-        }
-        return prefix;
-    }
-    public int[] suffixMax(int arr[]){
-        int n=arr.length;
-        int suffix[]=new int[n];
-        suffix[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=Math.max(suffix[i+1],arr[i]);
-        }
-        return suffix;
-    }
     public int trap(int[] height) {
-        int total=0;
         int n=height.length;
-        int preMax[] = prefixMax(height);
-        int suMax[] = suffixMax(height);
-        for(int i=0;i<n;i++){
-            int leftMax=preMax[i];
-            int rightMax=suMax[i];
-            if(height[i]<leftMax && height[i]<rightMax){
-                total+=Math.min(leftMax,rightMax)-height[i];
-            }
+        int prefix[]=new int[n];
+        prefix[0]=height[0];
+        //build maxium prefix array
+        for(int i=1;i<n;i++){
+            prefix[i]=Math.max(prefix[i-1],height[i]);
+        }
+        int rightMax=0;
+        int total=0;
+        for(int i=n-1;i>=0;i--){
+            rightMax=Math.max(rightMax,height[i]);
+            int leftMax=prefix[i];
+            total+=Math.min(leftMax,rightMax)-height[i];
         }
         return total;
     }
